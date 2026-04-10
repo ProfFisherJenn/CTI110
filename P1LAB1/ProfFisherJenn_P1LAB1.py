92c1c9fbdb4243e8ac086a6469bdfcdb49d8e0fd
ProfFisherJenn <fisherjl@faytechcc.edu>
2026-04-10 10:50:43 -0400

    Add files via upload

---
+++ P1LAB1/ProfFisherJenn_P1LAB1.py
@@ -0,0 +1,8 @@
+# Jennifer Fisher
+# April 10, 2026 sorry I'm late!
+# P1LAB1
+# Project to get user input to display at output
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+print("Welcome " + firstname + " " + lastname + " to CTI 110")
